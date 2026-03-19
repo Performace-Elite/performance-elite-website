e0f692b6211d9fc9164b344b2fcb581fd351409a
Performace-Elite <performanceelitevolleyball@gmail.com>
2026-03-19 08:25:58 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App, { ThankYouPage, AboutPage } from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
